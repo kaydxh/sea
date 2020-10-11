775d25e3b91a31c9df188f4f09b1330c32836038
kaydxh <xiao_hua_ding@sina.cn>
2020-10-11 16:22:56 +0800

    [ADD] add cmd

---
+++ cmd/sea/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/kaydxh/sea/pkg/cmd/sea"
+	"os"
+)
+
+func main() {
+
+	command := sea.NewProxyCommand()
+
+	//	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
+	//	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	// utilflag.InitFlags()
+	//	logs.InitLogs()
+	//	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
