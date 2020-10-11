6660b4c8b477d0ce6ab5bf997672a1b3c736a4b7
kaydxh <xiao_hua_ding@sina.cn>
2020-10-11 19:32:35 +0800

    [ADD] add script for generating  pb code

--- api/scripts/go_proto_gen.sh
+++ api/scripts/go_proto_gen.sh
@@ -2,16 +2,15 @@
 #!/usr/bin/env bash
 
 # Fail on any error.
-set -eo pipefail
+set -euo pipefail
 
 SCRIPTPATH=$(cd `dirname $0`;pwd)
 
-#:<<EOF
-#SCRIPTPATH=$(dirname "$SCRIPT")
-#SCRIPT=$(readlink -f "$0")
-#SCRIPTPATH=$(dirname "$SCRIPT")
-#echo ${SCRIPTPATH}
-#EOF
+<<'COMMENT'
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo ${SCRIPTPATH}
+COMMENT
 
 PROTOC_FILE_DIR="$1"
 
@@ -29,15 +28,21 @@ function die() {
     github.com/golang/protobuf/protoc-gen-go
 COMMENT
 
+echo `pwd`
+
 GEN_PROTO_TOOLS=(protoc protoc-gen-go protoc-gen-grpc-gateway protoc-gen-govalidators)
 for tool in ${GEN_PROTO_TOOLS[@]}; do
    q=$(command -v ${tool}) || die "didn't find ${tool}"
    echo 1>&2 "${tool}: ${q}"
 done
 
+proto_headers="-I ${SCRIPTPATH}/../../third_party"
+#proto_headers="${proto_headers} -I ${SCRIPTPATH}/../"
+proto_headers="${proto_headers} -I ${SCRIPTPATH}/../../third_party/github.com/grpc-ecosystem/grpc-gateway"
 grpc_option="--grpc-gateway_out=logtostderr=true"
+grpc_option="${grpc_option},paths=source_relative:."
 
 for f in $(find ${PROTOC_FILE_DIR} -type f -name '*.proto' -print0 | xargs -0); do
-  protoc -I . --govalidators_out=paths=source_relative:. --go_out=plugins=grpc,paths=source_relative:. ${grpc_option} ${f}
+  protoc -I . ${proto_headers} --govalidators_out=paths=source_relative:. --go_out=plugins=grpc,paths=source_relative:. ${grpc_option} ${f}
   echo $f
 done
