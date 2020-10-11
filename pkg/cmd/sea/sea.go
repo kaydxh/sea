9b52df34a1dd46a812e9c511ee891a68163224b3
kaydxh <xiao_hua_ding@sina.cn>
2020-10-11 16:24:31 +0800

    [ADD] add sea pkg

---
+++ pkg/cmd/sea/sea.go
@@ -0,0 +1,23 @@
+package sea
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func NewCommand(name string) *cobra.Command {
+
+	c := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of Hugo",
+		Long:  `All software has versions. This is Hugo's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+		},
+	}
+
+	c.AddCommand()
+	cobra.OnInitialize(func() {})
+
+	return c
+}
