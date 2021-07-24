d83f1c1bd4d58d9444b30ff40256716cc4aa22fb
kaydxh <523520531@qq.com>
2021-07-24 13:23:26 +0800

    build(script): auto build proto file

--- api/scripts/go_proto_gen.sh
+++ api/scripts/go_proto_gen.sh
@@ -5,12 +5,12 @@
 set -euo pipefail
 # set -o xtrace
 
-SCRIPTPATH=$(cd `dirname $0`;pwd)
+SCRIPT_PATH=$(cd `dirname $0`;pwd)
 
 <<'COMMENT'
 SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-echo ${SCRIPTPATH}
+SCRIPT_PATH=$(dirname "$SCRIPT")
+echo ${SCRIPT_PATH}
 COMMENT
 
 PROTOC_FILE_DIR="$1"
@@ -31,21 +31,32 @@ COMMENT
 
 echo `pwd`
 
+echo "==> Checking tools..."
 GEN_PROTO_TOOLS=(protoc protoc-gen-go protoc-gen-grpc-gateway protoc-gen-govalidators)
 for tool in ${GEN_PROTO_TOOLS[@]}; do
    q=$(command -v ${tool}) || die "didn't find ${tool}"
    echo 1>&2 "${tool}: ${q}"
 done
 
-proto_headers="-I ${SCRIPTPATH}/../../third_party"
-#proto_headers="${proto_headers} -I ${SCRIPTPATH}/../"
-proto_headers="${proto_headers} -I ${SCRIPTPATH}/../../third_party/github.com/grpc-ecosystem/grpc-gateway"
-grpc_option="--grpc-gateway_out=logtostderr=true"
-grpc_option="${grpc_option},paths=source_relative:."
 
-for f in $(find ${PROTOC_FILE_DIR} -type f -name '*.proto' -print0 | xargs -0); do
+echo "==> Generating proto..."
+proto_headers="-I ${SCRIPT_PATH}/../../third_party"
+proto_headers="${proto_headers} -I ${SCRIPT_PATH}/../../third_party/github.com/grpc-ecosystem/grpc-gateway"
+source_relative_option="paths=source_relative:."
+go_tag_option="--go-tag_out=${source_relative_option}"
+go_grpc_option="--go-grpc_out=${source_relative_option}"
+grpc_gateway_out_option="--grpc-gateway_out=logtostderr=true"
+grpc_gateway_delete_option="--grpc-gateway_opt=allow_delete_body=true"
+
+for proto in $(find ${PROTOC_FILE_DIR} -type f -name '*.proto' -print0 | xargs -0); do
   #protoc -I . ${proto_headers} --govalidators_out=paths=source_relative:. --go-grpc_out=plugins=grpc,paths=source_relative:. ${grpc_option} ${f}
-  #protoc -I . ${proto_headers} --govalidators_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./api/openapi-spec/v1.0/date/date.yaml,paths=source_relative:. --grpc-gateway_opt=allow_delete_body=true ${f}
-  protoc -I . ${proto_headers} --go-tag_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./api/openapi-spec/v1.0/date/date.yaml,paths=source_relative:. --grpc-gateway_opt=allow_delete_body=true ${f}
-  echo $f
+  echo "Generating ${proto}"
+  api_conf_yaml_base_name="$(basename ${proto} .proto).yaml"
+  api_conf_yaml_dir="$(dirname ${proto})"
+  api_conf_yaml="${api_conf_yaml_dir}/$api_conf_yaml_base_name"
+  grpc_api_yaml_option="grpc_api_configuration=${api_conf_yaml},${source_relative_option}"
+
+  grpc_option="${grpc_gateway_out_option},${grpc_api_yaml_option} ${grpc_gateway_delete_option}"
+  protoc -I . ${proto_headers} ${go_tag_option} ${go_grpc_option} ${grpc_option} "${proto}"
+  #protoc -I . ${proto_headers} --go-tag_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. --grpc-gateway_out=logtostderr=true,grpc_api_configuration=${api_conf_yaml},paths=source_relative:. --grpc-gateway_opt=allow_delete_body=true ${f}
 done
