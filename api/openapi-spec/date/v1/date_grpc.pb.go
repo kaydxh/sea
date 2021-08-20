228b0be18d282cdab5a87117e7e6d56b0373ecff
kaydxh <523520531@qq.com>
2021-08-20 11:04:50 +0800

    feat(api): date proto rename json tag to Date

--- api/openapi-spec/date/v1/date_grpc.pb.go
+++ api/openapi-spec/date/v1/date_grpc.pb.go
@@ -97,5 +97,5 @@ var DateService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "api/openapi-spec/v1/date/date.proto",
+	Metadata: "api/openapi-spec/date/v1/date.proto",
 }
