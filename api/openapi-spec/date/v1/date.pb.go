a4da5f653316856511d43b2c84ee924926607261
kayxhding <kayxhding@tencent.com>
2021-08-20 11:04:50 +0800

    feat(api): date proto rename json tag to Date

--- api/openapi-spec/date/v1/date.pb.go
+++ api/openapi-spec/date/v1/date.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.27.0
 // 	protoc        v3.13.0
-// source: api/openapi-spec/v1/date/date.proto
+// source: api/openapi-spec/date/v1/date.proto
 
 package date
 
@@ -32,7 +32,7 @@ type DateRequest struct {
 func (x *DateRequest) Reset() {
 	*x = DateRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_openapi_spec_v1_date_date_proto_msgTypes[0]
+		mi := &file_api_openapi_spec_date_v1_date_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -45,7 +45,7 @@ func (x *DateRequest) String() string {
 func (*DateRequest) ProtoMessage() {}
 
 func (x *DateRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_api_openapi_spec_v1_date_date_proto_msgTypes[0]
+	mi := &file_api_openapi_spec_date_v1_date_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -58,7 +58,7 @@ func (x *DateRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use DateRequest.ProtoReflect.Descriptor instead.
 func (*DateRequest) Descriptor() ([]byte, []int) {
-	return file_api_openapi_spec_v1_date_date_proto_rawDescGZIP(), []int{0}
+	return file_api_openapi_spec_date_v1_date_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *DateRequest) GetRequestId() string {
@@ -74,13 +74,13 @@ type DateResponse struct {
 	unknownFields protoimpl.UnknownFields
 
 	RequestId string `protobuf:"bytes,1,opt,name=request_id,json=RequestId,proto3" json:"request_id,omitempty"`
-	Date      string `protobuf:"bytes,2,opt,name=date,proto3" json:"date,omitempty"`
+	Date      string `protobuf:"bytes,2,opt,name=date,json=Date,proto3" json:"date,omitempty"`
 }
 
 func (x *DateResponse) Reset() {
 	*x = DateResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_openapi_spec_v1_date_date_proto_msgTypes[1]
+		mi := &file_api_openapi_spec_date_v1_date_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -93,7 +93,7 @@ func (x *DateResponse) String() string {
 func (*DateResponse) ProtoMessage() {}
 
 func (x *DateResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_api_openapi_spec_v1_date_date_proto_msgTypes[1]
+	mi := &file_api_openapi_spec_date_v1_date_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -106,7 +106,7 @@ func (x *DateResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use DateResponse.ProtoReflect.Descriptor instead.
 func (*DateResponse) Descriptor() ([]byte, []int) {
-	return file_api_openapi_spec_v1_date_date_proto_rawDescGZIP(), []int{1}
+	return file_api_openapi_spec_date_v1_date_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *DateResponse) GetRequestId() string {
@@ -123,11 +123,11 @@ func (x *DateResponse) GetDate() string {
 	return ""
 }
 
-var File_api_openapi_spec_v1_date_date_proto protoreflect.FileDescriptor
+var File_api_openapi_spec_date_v1_date_proto protoreflect.FileDescriptor
 
-var file_api_openapi_spec_v1_date_date_proto_rawDesc = []byte{
+var file_api_openapi_spec_date_v1_date_proto_rawDesc = []byte{
 	0x0a, 0x23, 0x61, 0x70, 0x69, 0x2f, 0x6f, 0x70, 0x65, 0x6e, 0x61, 0x70, 0x69, 0x2d, 0x73, 0x70,
-	0x65, 0x63, 0x2f, 0x76, 0x31, 0x2f, 0x64, 0x61, 0x74, 0x65, 0x2f, 0x64, 0x61, 0x74, 0x65, 0x2e,
+	0x65, 0x63, 0x2f, 0x64, 0x61, 0x74, 0x65, 0x2f, 0x76, 0x31, 0x2f, 0x64, 0x61, 0x74, 0x65, 0x2e,
 	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0f, 0x73, 0x65, 0x61, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x76,
 	0x31, 0x2e, 0x64, 0x61, 0x74, 0x65, 0x1a, 0x20, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x70,
 	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74,
@@ -138,7 +138,7 @@ var file_api_openapi_spec_v1_date_date_proto_rawDesc = []byte{
 	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x1d, 0x0a, 0x0a, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73,
 	0x74, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x52, 0x65, 0x71, 0x75,
 	0x65, 0x73, 0x74, 0x49, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x65, 0x18, 0x02, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x04, 0x64, 0x61, 0x74, 0x65, 0x32, 0x53, 0x0a, 0x0b, 0x44, 0x61, 0x74,
+	0x01, 0x28, 0x09, 0x52, 0x04, 0x44, 0x61, 0x74, 0x65, 0x32, 0x53, 0x0a, 0x0b, 0x44, 0x61, 0x74,
 	0x65, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x44, 0x0a, 0x03, 0x4e, 0x6f, 0x77, 0x12,
 	0x1c, 0x2e, 0x73, 0x65, 0x61, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x76, 0x31, 0x2e, 0x64, 0x61, 0x74,
 	0x65, 0x2e, 0x44, 0x61, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x1d, 0x2e,
@@ -151,23 +151,23 @@ var file_api_openapi_spec_v1_date_date_proto_rawDesc = []byte{
 }
 
 var (
-	file_api_openapi_spec_v1_date_date_proto_rawDescOnce sync.Once
-	file_api_openapi_spec_v1_date_date_proto_rawDescData = file_api_openapi_spec_v1_date_date_proto_rawDesc
+	file_api_openapi_spec_date_v1_date_proto_rawDescOnce sync.Once
+	file_api_openapi_spec_date_v1_date_proto_rawDescData = file_api_openapi_spec_date_v1_date_proto_rawDesc
 )
 
-func file_api_openapi_spec_v1_date_date_proto_rawDescGZIP() []byte {
-	file_api_openapi_spec_v1_date_date_proto_rawDescOnce.Do(func() {
-		file_api_openapi_spec_v1_date_date_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_openapi_spec_v1_date_date_proto_rawDescData)
+func file_api_openapi_spec_date_v1_date_proto_rawDescGZIP() []byte {
+	file_api_openapi_spec_date_v1_date_proto_rawDescOnce.Do(func() {
+		file_api_openapi_spec_date_v1_date_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_openapi_spec_date_v1_date_proto_rawDescData)
 	})
-	return file_api_openapi_spec_v1_date_date_proto_rawDescData
+	return file_api_openapi_spec_date_v1_date_proto_rawDescData
 }
 
-var file_api_openapi_spec_v1_date_date_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_api_openapi_spec_v1_date_date_proto_goTypes = []interface{}{
+var file_api_openapi_spec_date_v1_date_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_api_openapi_spec_date_v1_date_proto_goTypes = []interface{}{
 	(*DateRequest)(nil),  // 0: sea.api.v1.date.DateRequest
 	(*DateResponse)(nil), // 1: sea.api.v1.date.DateResponse
 }
-var file_api_openapi_spec_v1_date_date_proto_depIdxs = []int32{
+var file_api_openapi_spec_date_v1_date_proto_depIdxs = []int32{
 	0, // 0: sea.api.v1.date.DateService.Now:input_type -> sea.api.v1.date.DateRequest
 	1, // 1: sea.api.v1.date.DateService.Now:output_type -> sea.api.v1.date.DateResponse
 	1, // [1:2] is the sub-list for method output_type
@@ -177,13 +177,13 @@ var file_api_openapi_spec_v1_date_date_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_api_openapi_spec_v1_date_date_proto_init() }
-func file_api_openapi_spec_v1_date_date_proto_init() {
-	if File_api_openapi_spec_v1_date_date_proto != nil {
+func init() { file_api_openapi_spec_date_v1_date_proto_init() }
+func file_api_openapi_spec_date_v1_date_proto_init() {
+	if File_api_openapi_spec_date_v1_date_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_api_openapi_spec_v1_date_date_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_api_openapi_spec_date_v1_date_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*DateRequest); i {
 			case 0:
 				return &v.state
@@ -195,7 +195,7 @@ func file_api_openapi_spec_v1_date_date_proto_init() {
 				return nil
 			}
 		}
-		file_api_openapi_spec_v1_date_date_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_api_openapi_spec_date_v1_date_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*DateResponse); i {
 			case 0:
 				return &v.state
@@ -212,18 +212,18 @@ func file_api_openapi_spec_v1_date_date_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_api_openapi_spec_v1_date_date_proto_rawDesc,
+			RawDescriptor: file_api_openapi_spec_date_v1_date_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_api_openapi_spec_v1_date_date_proto_goTypes,
-		DependencyIndexes: file_api_openapi_spec_v1_date_date_proto_depIdxs,
-		MessageInfos:      file_api_openapi_spec_v1_date_date_proto_msgTypes,
+		GoTypes:           file_api_openapi_spec_date_v1_date_proto_goTypes,
+		DependencyIndexes: file_api_openapi_spec_date_v1_date_proto_depIdxs,
+		MessageInfos:      file_api_openapi_spec_date_v1_date_proto_msgTypes,
 	}.Build()
-	File_api_openapi_spec_v1_date_date_proto = out.File
-	file_api_openapi_spec_v1_date_date_proto_rawDesc = nil
-	file_api_openapi_spec_v1_date_date_proto_goTypes = nil
-	file_api_openapi_spec_v1_date_date_proto_depIdxs = nil
+	File_api_openapi_spec_date_v1_date_proto = out.File
+	file_api_openapi_spec_date_v1_date_proto_rawDesc = nil
+	file_api_openapi_spec_date_v1_date_proto_goTypes = nil
+	file_api_openapi_spec_date_v1_date_proto_depIdxs = nil
 }
