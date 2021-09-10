73116a8820a2b3b3adfa1b52ca761e00d04a70c6
kaydxh <523520531@qq.com>
2021-09-10 11:40:23 +0800

    feat(database): add operator for task table

---
+++ pkg/sealet/database/dao/task.dao.go
@@ -0,0 +1,65 @@
+package dao
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	mysql_ "github.com/kaydxh/golang/pkg/database/mysql"
+	"github.com/kaydxh/sea/pkg/sealet/database/model"
+)
+
+type TaskDao struct{}
+
+// AddTask
+func (dao TaskDao) AddTask(ctx context.Context, db *sqlx.DB, arg model.Task) error {
+
+	query := `INSERT INTO task 
+			 (is_deleted, task_name, task_id, task_type, task_status)
+			 VALUES (:is_deleted,
+			         :task_name,
+					 :task_id,
+					 :task_type,
+					 :task_status)
+					 ON DUPLICATE KEY UPDATE
+					 is_deleted   = :is_deleted,
+                     task_name    = :task_name,
+                     task_id      = :task_id,
+                     task_type    = :task_type,
+                     task_status  = :task_status
+					 `
+	return arg.ExecTaskByQuery(ctx, db, query)
+}
+
+// DeleteTask
+func (dao TaskDao) DeleteTask(ctx context.Context, db *sqlx.DB, arg model.Task) error {
+	query := `DELETE FROM task`
+	return arg.ExecTaskByQuery(
+		ctx,
+		db,
+		mysql_.GenerateCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, query, arg),
+	)
+}
+
+// UpdateTask
+// UPDATE task SET foo=:foo, bar=:bar WHERE thud=:thud AND grunt=:grunt
+func (dao TaskDao) UpdateTask(ctx context.Context, db *sqlx.DB, cols, conds []string, arg model.Task) error {
+	query := fmt.Sprintf(
+		`UPDATE task SET %s %s`,
+		mysql_.JoinNamedColumnsValues(cols...),
+		mysql_.GenerateNameColumsCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, conds...),
+	)
+
+	return arg.ExecTaskByQuery(ctx, db, query)
+}
+
+// GetTasks
+func (dao TaskDao) GetTasks(ctx context.Context, db *sqlx.DB, arg model.Task) ([]model.Task, error) {
+	query := `SELECT * FROM task`
+	return arg.GetTasksByQuery(
+		ctx,
+		db,
+		mysql_.GenerateCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, query, arg),
+	)
+
+}
---
+++ pkg/sealet/database/model/task.model.go
@@ -0,0 +1,52 @@
+package model
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Task struct {
+	Id sql.NullInt64 `db:"id"` // primary key ID
+
+	// NullTime represents a time.Time that may be null.
+	// NullTime implements the Scanner interface so
+	// it can be used as a scan destination, similar to NullString.
+	CreatedAt sql.NullTime `db:"created_at"`
+	UpdatedAt sql.NullTime `db:"updated_at"`
+
+	IsDeleted bool         `db:"is_deleted"` // soft delete, 0 for not deleted, 1 for deleted
+	DeletedAt sql.NullTime `db:"deleted_at"`
+	Version   int          `db:"version"`
+
+	TaskName   string `db:"task_name"`
+	TaskId     string `db:"task_id"`
+	TaskType   int    `db:"task_type"`
+	TaskStatus int    `db:"task_status"`
+}
+
+func (arg Task) GetTasksByQuery(ctx context.Context, db *sqlx.DB, query string) ([]Task, error) {
+	// Check that invalid preparations fail
+	ns, err := db.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer ns.Close()
+
+	var dest []Task
+	err = ns.SelectContext(ctx, &dest, arg)
+	if err != nil {
+		return nil, err
+	}
+	return dest, nil
+}
+
+// exec sql for insert/update/delete
+func (arg Task) ExecTaskByQuery(ctx context.Context, db *sqlx.DB, query string) error {
+	_, err := db.NamedExecContext(ctx, query, arg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
