181eec2aa702ee54a513229eeff1ab044055946a
kayxhding <kayxhding@tencent.com>
2021-09-13 08:48:58 +0800

    style(dao benchmark): test add task

--- pkg/sealet/database/dao/dao_benchmark_test.go
+++ pkg/sealet/database/dao/dao_benchmark_test.go
@@ -2,6 +2,7 @@ package dao_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
@@ -9,14 +10,19 @@ import (
 	"github.com/kaydxh/sea/pkg/sealet/database/model"
 )
 
+// -count the benchmark times, -benchtime the test execute times(用例执行次数) or execute time(用例执行时间)
+//go test -bench="AddTask" -benchtime=5s -count=3 .
+//go test -bench="AddTask" -benchtime=50x -count=3 .
 func BenchmarkAddTask(t *testing.B) {
-
-	err := dao.TaskDao{}.AddTask(context.Background(), GetDBOrDie(), model.Task{
-		TaskName: "task3",
-		TaskId:   uuid.New().String(),
-		TaskType: 3,
-	})
-	if err != nil {
-		t.Fatalf("failed to add tasks, err: %v", err)
+	for n := 0; n < t.N; n++ {
+		fmt.Println("n: ", n)
+		err := dao.TaskDao{}.AddTask(context.Background(), GetDBOrDie(), model.Task{
+			TaskName: "task3",
+			TaskId:   uuid.New().String(),
+			TaskType: 3,
+		})
+		if err != nil {
+			t.Fatalf("failed to add tasks, err: %v", err)
+		}
 	}
 }
