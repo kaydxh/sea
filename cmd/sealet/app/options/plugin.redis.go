29b655832496f8b35d6fbc68ae192c699bd979aa
kaydxh <523520531@qq.com>
2021-09-18 21:20:36 +0800

    feat(plugin): install mysql and redis with context, that canceld with signal

--- cmd/sealet/app/options/plugin.redis.go
+++ cmd/sealet/app/options/plugin.redis.go
@@ -1,17 +1,19 @@
 package options
 
 import (
+	"context"
+
 	"github.com/kaydxh/sea/pkg/sealet/provider"
 	"github.com/sirupsen/logrus"
 )
 
-func (s *CompletedServerRunOptions) installRedisOrDie() {
+func (s *CompletedServerRunOptions) installRedisOrDie(ctx context.Context) {
 	c := s.redisConfig.Complete()
 	if !c.Proto.GetEnabled() {
 		return
 	}
 
-	db, err := c.New()
+	db, err := c.New(ctx)
 	if err != nil {
 		logrus.WithError(err).Fatalf("install Redis, exit")
 		return
