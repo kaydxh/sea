60768c89c832c867ab442059ae12e15726b81f9d
kaydxh <523520531@qq.com>
2021-09-25 22:13:52 +0800

    feat(redis): add task to redis

--- pkg/sealet/database/dao/redis.dao/task.dao.go
+++ pkg/sealet/database/dao/redis.dao/task.dao.go
@@ -8,6 +8,7 @@ import (
 	runtime_ "github.com/kaydxh/golang/go/runtime"
 	time_ "github.com/kaydxh/golang/go/time"
 	database_ "github.com/kaydxh/golang/pkg/database"
+	redis_ "github.com/kaydxh/golang/pkg/database/redis"
 
 	"github.com/kaydxh/sea/pkg/sealet/database/dao"
 	"github.com/kaydxh/sea/pkg/sealet/database/model"
@@ -24,6 +25,7 @@ func NewTaskDao(db *redis.Client) *TaskDao {
 
 // AddTask
 func (d *TaskDao) AddTask(ctx context.Context, arg model.Task) error {
+
 	tc := time_.New(true)
 	caller := runtime_.GetShortCaller()
 	logger := logrus.WithField("caller", caller)
@@ -38,12 +40,12 @@ func (d *TaskDao) AddTask(ctx context.Context, arg model.Task) error {
 	ctx, cancel := database_.WithDatabaseExecuteTimeout(ctx, dao.DatabaseExecuteTimeout)
 	defer cancel()
 
-	res, err := d.db.HSet(ctx, arg.TaskId, "task_id", arg.TaskId).Result()
+	err := redis_.HSetStruct(ctx, d.db, arg.TaskId, arg)
 	if err != nil {
 		return err
 	}
 
-	logger.WithField("cost", tc.String()).Infof("result of HSet: %v", res)
+	logger.WithField("cost", tc.String()).Infof("successed HSet")
 
 	return nil
 }
