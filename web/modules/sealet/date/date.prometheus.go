068f7330ee7006539e7fa190d018b0ca2137abcd
kaydxh <523520531@qq.com>
2021-10-01 15:03:42 +0800

    feat(prometheus): prometheus for date interface

---
+++ web/modules/sealet/date/date.prometheus.go
@@ -0,0 +1,31 @@
+// Copyright 2020 The kaydxh Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package date
+
+/*
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	httpReqestLabelClientIP = "client_ip"
+)
+
+var (
+	nowProcessed = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "method_now_http_request_total",
+		Help: "the total number of processed now http requests.",
+	},
+		[]string{httpReqestLabelClientIP},
+	)
+
+	nowCost = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "method_now_http_request_cost_duration_milliseconds",
+			Help: "the duration of method now cost in milliseconds.",
+		})
+)
+*/
