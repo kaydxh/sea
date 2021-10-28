6866140be96a49c75958580029148be75b41a19b
kaydxh <523520531@qq.com>
2021-10-28 23:12:49 +0800

    feat(database): add get count

--- pkg/sealet/database/dao/mysql.dao/task.dao.go
+++ pkg/sealet/database/dao/mysql.dao/task.dao.go
@@ -97,6 +97,20 @@ func (d *TaskDao) GetTasks(ctx context.Context, arg model.Task) ([]model.Task, e
 
 }
 
+// GetTaskCount
+func (d *TaskDao) GetTaskCount(ctx context.Context, conds []string, arg model.Task) (uint64, error) {
+
+	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
+	defer cancel()
+
+	query := fmt.Sprintf(
+		`SELECT count(*) FROM ti_model_repository %s`,
+		mysql_.GenerateNameColumsCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, conds...),
+	)
+
+	return d.getCountByQuery(ctx, query, arg)
+}
+
 func (d *TaskDao) getTasksByQuery(ctx context.Context, query string, arg model.Task) ([]model.Task, error) {
 	tc := time_.New(true)
 	caller := runtime_.GetShortCaller()
@@ -120,6 +134,30 @@ func (d *TaskDao) getTasksByQuery(ctx context.Context, query string, arg model.T
 		return nil, err
 	}
 	return dest, nil
+}
+
+func (d *TaskDao) getCountByQuery(ctx context.Context, query string, arg model.Task) (uint64, error) {
+	tc := time_.New(true)
+	caller := runtime_.GetShortCaller()
+	logger := logrus.WithField("caller", caller)
+	clean := func() {
+		tc.Tick(caller)
+		logger.WithField("cost", tc.String()).Infof("SQL EXECL")
+	}
+	defer clean()
+
+	ns, err := d.db.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return 0, err
+	}
+
+	var count uint64
+	err = ns.QueryRowContext(ctx, arg).Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
 }
 
 // exec sql for insert/update/delete
