7ba8ce6a841dffdeb77c140485b00f08fe3c4650
kayxhding <kayxhding@tencent.com>
2021-12-10 17:05:25 +0800

    refactor(mysql): dao

--- pkg/sealet/database/dao/mysql.dao/task.dao.go
+++ pkg/sealet/database/dao/mysql.dao/task.dao.go
@@ -14,6 +14,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const taskTableName = "task"
+
 type TaskDao struct {
 	db *sqlx.DB
 }
@@ -28,7 +30,7 @@ func (d *TaskDao) AddTask(ctx context.Context, arg *model.Task) error {
 	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
 	defer cancel()
 
-	query := `INSERT INTO task 
+	query := fmt.Sprintf(`INSERT INTO %s
 			   (
 			   is_deleted,
 			   task_name,
@@ -48,23 +50,24 @@ func (d *TaskDao) AddTask(ctx context.Context, arg *model.Task) error {
                      task_name    = :task_name,
                      task_id      = :task_id,
                      task_type    = :task_type,
-                     task_status  = :task_status`
+                     task_status  = :task_status`, taskTableName)
 
-	return d.execTaskByQuery(ctx, query, arg)
+	return mysql_.ExecContext(ctx, query, arg, nil, d.db)
 }
 
 // DeleteTask
-func (d *TaskDao) DeleteTask(ctx context.Context, arg *model.Task) error {
+func (d *TaskDao) DeleteTask(ctx context.Context, conds []string, arg *model.Task) error {
 
 	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
 	defer cancel()
 
-	query := `DELETE FROM task`
-	return d.execTaskByQuery(
-		ctx,
-		mysql_.GenerateCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, query, arg),
-		arg,
-	)
+	query := fmt.Sprintf(
+		`DELETE FROM %s
+	           WHERE %s`,
+		taskTableName,
+		mysql_.ConditionWithEqualAnd(conds...))
+
+	return mysql_.ExecContext(ctx, query, arg, nil, d.db)
 }
 
 // UpdateTask
@@ -75,58 +78,67 @@ func (d *TaskDao) UpdateTask(ctx context.Context, cols, conds []string, arg *mod
 	defer cancel()
 
 	query := fmt.Sprintf(
-		`UPDATE task SET %s %s`,
+		`UPDATE %s
+		    SET %s
+	      WHERE %s`,
+		taskTableName,
 		mysql_.JoinNamedColumnsValues(cols...),
-		mysql_.GenerateNameColumsCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, conds...),
+		mysql_.ConditionWithEqualAnd(conds...),
 	)
-	return d.execTaskByQuery(ctx, query, arg)
+	return mysql_.ExecContext(ctx, query, arg, nil, d.db)
 }
 
 // GetTasks
-func (d *TaskDao) GetTasks(ctx context.Context, arg *model.Task) ([]*model.Task, error) {
-
-	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
-	defer cancel()
-
-	query := `SELECT * FROM task`
-	return d.getTasksByQuery(
-		ctx,
-		mysql_.GenerateCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, query, arg),
-		arg,
-	)
-
-}
-
-// GetTasks
-func (d *TaskDao) GetTasksLike(ctx context.Context, arg *model.Task) ([]*model.Task, error) {
-
-	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
-	defer cancel()
-
-	query := `SELECT * FROM task`
-	return d.getTasksByQuery(
-		ctx,
-		mysql_.GenerateCondition(mysql_.SqlCompareLike, mysql_.SqlOperatorAnd, query, arg),
-		arg,
-	)
-
-}
-
-// GetTaskCount
-func (d *TaskDao) GetTaskCount(ctx context.Context, conds []string, arg *model.Task) (uint64, error) {
+func (d *TaskDao) GetTasks(ctx context.Context, conds []string, arg *model.Task) ([]*model.Task, error) {
 
 	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
 	defer cancel()
 
 	query := fmt.Sprintf(
-		`SELECT count(*) FROM ti_model_repository %s`,
-		mysql_.GenerateNameColumsCondition(mysql_.SqlCompareEqual, mysql_.SqlOperatorAnd, conds...),
+		`SELECT * FROM %s
+	             WHERE %s`,
+		taskTableName,
+		mysql_.ConditionWithEqualAnd(conds...),
 	)
-
-	return d.getCountByQuery(ctx, query, arg)
+	return d.getTasks(ctx, query, arg)
 }
 
-func (d *TaskDao) getTasksByQuery(ctx context.Context, query string, arg *model.Task) (dest []*model.Task, err error) {
+func (d *TaskDao) GetTasksByPage(
+	ctx context.Context,
+	offset, limit int32,
+	conds []string,
+	arg map[string]interface{},
+) ([]*model.Task, error) {
+	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
+	defer cancel()
+
+	query := fmt.Sprintf(
+		`SELECT * FROM  %s
+		     WHERE %s
+			   ORDER BY create_time DESC, id DESC limit %d, %d`,
+		taskTableName,
+		mysql_.ConditionWithEqualAnd(conds...),
+	)
+	return m.getTasks(ctx, query, arg)
+}
+
+// GetTasksCount
+func (d *TaskDao) GetTasksCount(ctx context.Context, conds []string, arg *model.Task) (uint64, error) {
+
+	ctx, cancel := context_.WithTimeout(ctx, dao.DatabaseExecuteTimeout)
+	defer cancel()
+
+	query := fmt.Sprintf(
+		`SELECT count(*) FROM %s
+		                WHERE %s`,
+		taskTableName,
+		mysql_.ConditionWithEqualAnd(conds...),
+	)
+
+	return mysql_.GetCountContext(ctx, query, arg, d.db)
+}
+
+func (d *TaskDao) getTasks(ctx context.Context, query string, arg interface{}) (dest []*model.Task, err error) {
 	tc := time_.New(true)
 	caller := runtime_.GetShortCaller()
 	logger := logrus.WithField("caller", caller)
@@ -150,54 +162,25 @@ func (d *TaskDao) getTasksByQuery(ctx context.Context, query string, arg *model.
 	if err != nil {
 		return nil, err
 	}
+	/*
+		rows, err := d.db.NamedQueryContext(ctx, query, arg)
+		if err != nil {
+			fmt.Printf("===err: %v\n", err)
+			return nil, err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var task model.Task
+			err = rows.StructScan(&task)
+			if err != nil {
+				fmt.Printf("StructScan===err: %v\n", err)
+				return nil, err
+			}
+
+			dest = append(dest, &task)
+		}
+	*/
+
 	return dest, nil
-}
-
-func (d *TaskDao) getCountByQuery(ctx context.Context, query string, arg *model.Task) (count uint64, err error) {
-	tc := time_.New(true)
-	caller := runtime_.GetShortCaller()
-	logger := logrus.WithField("caller", caller)
-	clean := func() {
-		tc.Tick(caller)
-		logger.WithField("cost", tc.String()).Infof("SQL EXECL")
-		if err != nil {
-			logger.WithError(err).Errorf("sql: %s", query)
-		}
-	}
-	defer clean()
-
-	ns, err := d.db.PrepareNamedContext(ctx, query)
-	if err != nil {
-		return 0, err
-	}
-
-	err = ns.QueryRowContext(ctx, arg).Scan(&count)
-	if err != nil {
-		return 0, err
-	}
-
-	return count, nil
-}
-
-// exec sql for insert/update/delete
-func (d *TaskDao) execTaskByQuery(ctx context.Context, query string, arg *model.Task) (err error) {
-	tc := time_.New(true)
-	caller := runtime_.GetShortCaller()
-	logger := logrus.WithField("caller", caller)
-	clean := func() {
-		tc.Tick(caller)
-		logger.WithField("cost", tc.String()).Infof("SQL EXECL")
-		if err != nil {
-			logger.WithError(err).Errorf("sql: %s", query)
-		}
-	}
-	defer clean()
-
-	rows, err := d.db.NamedExecContext(ctx, query, arg)
-	if err != nil {
-		return err
-	}
-	logger.Infof("affected rows: %v by sql", rows)
-
-	return nil
 }
