fa9354328f75f5e0eb8f816cd3896621984cace3
kaydxh <523520531@qq.com>
2022-04-26 22:35:53 +0800

    build(build.sh): add git status

--- script/build.sh
+++ script/build.sh
@@ -1,14 +1,31 @@
 # Created by kayxhding on 2020-10-11 19:34:08
 #!/usr/bin/env bash
 
-# Fail on any error
-set -euo pipefail
+# exit by command return non-zero exit code
+set -o errexit
+# Indicate an error when it encounters an undefined variable
+set -o nounset
+# Fail on any error.
+set -o pipefail
+# set -o xtrace
+
+#SEA_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
 # Get the git commit
-GIT_COMMIT=$(git rev-parse HEAD)
+# GIT="git --work-tree"
+GIT="git"
+GIT_COMMIT=$("${GIT}" rev-parse "HEAD^{commit}")
 GIT_TAG=$(git describe --long --tags --dirty --tags --always)
 GIT_BUILD_TIME=$(TZ=UTC date -u '+%Y-%m-%dT%H:%M:%SZ')
 GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
+
+GIT_TREE_STATE=${GIT_TREE_STATE-}
+if git_status=$("${GIT}" status --porcelain) && [[ -z ${git_status} ]]; then
+   GIT_TREE_STATE="clean"
+else
+   GIT_TREE_STATE="dirty"
+fi
+
 
 export CGO_ENABLED=0
 export GOFLAGS="-mod=readonly"
