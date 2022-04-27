982127db4f1b3b531a51ea0969462def27c1ed95
kaydxh <523520531@qq.com>
2022-04-27 12:52:56 +0800

    build(build): add git version

--- script/build.sh
+++ script/build.sh
@@ -9,37 +9,38 @@ set -o nounset
 set -o pipefail
 # set -o xtrace
 
-#SEA_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
+SEA_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+SEA_OUTPUT_SUB_PATH=${SEA_OUTPUT_SUB_PATH:-output}
+SEA_OUTPUT_PATH=${SEA_ROOT}/${SEA_OUTPUT_SUB_PATH}
+SEA_OUTPUT_BIN_PATH=${SEA_OUTPUT_PATH}/bin
 
-# Get the git commit
-# GIT="git --work-tree"
-GIT="git"
-GIT_COMMIT=$("${GIT}" rev-parse "HEAD^{commit}")
-GIT_TAG=$(git describe --long --tags --dirty --tags --always)
-GIT_BUILD_TIME=$(TZ=UTC date -u '+%Y-%m-%dT%H:%M:%SZ')
-GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
-
-GIT_TREE_STATE=${GIT_TREE_STATE-}
-if git_status=$("${GIT}" status --porcelain) && [[ -z ${git_status} ]]; then
-   GIT_TREE_STATE="clean"
-else
-   GIT_TREE_STATE="dirty"
-fi
-
+source "${SEA_ROOT}/script/version.sh"
 
 export CGO_ENABLED=0
 export GOFLAGS="-mod=readonly"
 
-OUT_PUT_PATH=output
-OUT_PUT_BIN_PATH=${OUT_PUT_PATH}/bin
-echo "==> Building..."
-rm -rf ${OUT_PUT_PATH}/*
-#go build -mod=vendor -o ${OUT_PUT_PATH}/sealet ./cmd/sealet
-go mod tidy
-go build -o ${OUT_PUT_BIN_PATH}/sealet ./cmd/sealet
-cp -rf conf ${OUT_PUT_PATH}
+function platform() {
+  echo "$(go env GOHOSTOS)/$(go env GOHOSTARCH)"
+}
+
+echo "==> Building in $(platform)"
+
+function build() {
+  local goldflags
+  goldflags="all=$(ldflags) ${GOLDFLAGS:-}"
+  echo "==${goldflags}"
+
+  rm -rf ${SEA_OUTPUT_PATH}/*
+  #go build -mod=vendor -o ${OUT_PUT_PATH}/sealet ./cmd/sealet
+  go mod tidy
+  go build -o ${SEA_OUTPUT_BIN_PATH}/sealet ./cmd/sealet
+  cp -rf conf ${SEA_OUTPUT_PATH}
+
+}
+
+build
 
 # Done!
 echo
 echo "==> Results:"
-ls -hl ${OUT_PUT_PATH}/
+ls -hl ${SEA_OUTPUT_PATH}/
---
+++ script/version.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# exit by command return non-zero exit code
+set -o errexit
+# Indicate an error when it encounters an undefined variable
+set -o nounset
+# Fail on any error.
+set -o pipefail
+# set -o xtrace
+
+function get_version_from_git() {
+  GIT="git"
+  GIT_COMMIT=$("${GIT}" rev-parse "HEAD^{commit}")
+  GIT_TAG=$(git describe --long --tags --dirty --tags --always)
+  GIT_BUILD_TIME=$(TZ=UTC date -u '+%Y-%m-%dT%H:%M:%SZ')
+
+
+  GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
+
+  GIT_TREE_STATE=${GIT_TREE_STATE-}
+  if git_status=$("${GIT}" status --porcelain) && [[ -z ${git_status} ]]; then
+     GIT_TREE_STATE="clean"
+  else
+     GIT_TREE_STATE="dirty"
+  fi
+}
+
+function ldflags() {
+  get_version_from_git
+
+  local -a ldflags
+  function add_ldflag() {
+    local key=${1}
+    local val=${2}
+
+    # update the list github.com/kaydxh/golang/pkg/app.
+    ldflags+=(
+      "-X 'github.com/kaydxh/golang/pkg/app.${key}=${val}'"
+    )
+  }
+
+ add_ldflag "buildDate" "${GIT_BUILD_TIME}"
+ add_ldflag "gitVersion" "${GIT_TAG}"
+ add_ldflag "gitCommit" "${GIT_COMMIT}"
+ add_ldflag "gitTreeState" "${GIT_TREE_STATE}"
+ echo "${ldflags[*]}"
+}
