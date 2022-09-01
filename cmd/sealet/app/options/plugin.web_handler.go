e8986ab4d612a2569f537958b74824d47648e30a
kaydxh <xiao_hua_ding@sina.cn>
2022-09-01 23:02:56 +0800

    feat(web handler): add profile

--- cmd/sealet/app/options/plugin.web_handler.go
+++ cmd/sealet/app/options/plugin.web_handler.go
@@ -2,9 +2,11 @@ package options
 
 import (
 	webserver_ "github.com/kaydxh/golang/pkg/webserver"
-	"github.com/kaydxh/sea/web/sealet"
+	"github.com/kaydxh/sea/web/modules/sealet/date"
+	sealet_ "github.com/kaydxh/sea/web/sealet"
 )
 
 func (s *CompletedServerRunOptions) installWebHandler(ws *webserver_.GenericWebServer) {
-	ws.InstallWebHandlers(sealet.NewHandler())
+	dateApp := date.NewController()
+	sealet_.NewWebHandlers(ws, dateApp)
 }
