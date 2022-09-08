7f1711da74dee97547b18e958fff623ef23f0211
kaydxh <523520531@qq.com>
2022-09-08 11:35:15 +0800

    test(sealet): test for date

---
+++ test/sealet/sealet_test.go
@@ -0,0 +1,55 @@
+package sealet_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	http_ "github.com/kaydxh/golang/go/net/http"
+	v1 "github.com/kaydxh/sea/api/openapi-spec/sealet/date/v1"
+)
+
+const (
+	serverAddr = "localhost:10001"
+	timeout    = 5 * time.Second
+)
+
+func TestHttpJsonNow(t *testing.T) {
+	testCases := []struct {
+		expected string
+	}{
+		{
+			expected: "",
+		},
+	}
+
+	client, err := http_.NewClient(http_.WithTimeout(timeout))
+	if err != nil {
+		t.Fatalf("failed to new http client got: %v", err)
+	}
+	for i, _ := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			req := &v1.DateRequest{
+				RequestId: uuid.NewString(),
+			}
+			dataReq, err := json.Marshal(req)
+			if err != nil {
+				t.Fatalf("failed to json marshal got: %v", err)
+			}
+
+			dataResp, err := client.PostJson("http://"+serverAddr+"/Now", nil, dataReq)
+			if err != nil {
+				t.Fatalf("failed to post json got: %v", err)
+			}
+
+			var resp v1.DateResponse
+			err = json.Unmarshal(dataResp, &resp)
+			if err != nil {
+				t.Fatalf("failed to json unmarshal got: %v", err)
+			}
+			t.Logf("response: %v", resp.String())
+		})
+	}
+}
