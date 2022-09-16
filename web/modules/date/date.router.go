e0a11f21dd7acfa56e74b2cc2f8763bd40a7a206
kaydxh <523520531@qq.com>
2022-09-16 08:28:17 +0800

    refactor(date): remove version

--- web/modules/date/date.router.go
+++ web/modules/date/date.router.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	gw_ "github.com/kaydxh/golang/pkg/grpc-gateway"
-	"github.com/kaydxh/sea/api/openapi-spec/date/v1"
+	"github.com/kaydxh/sea/api/openapi-spec/date"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
