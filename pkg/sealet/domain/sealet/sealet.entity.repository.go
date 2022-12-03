4d6892ac9317e292e8b03b33926f61a60ef8a363
kaydxh <xiao_hua_ding@sina.cn>
2022-12-03 16:09:33 +0800

    refactor(nowerror): use DDD design for NowError interface

--- pkg/sealet/domain/sealet/sealet.entity.repository.go
+++ pkg/sealet/domain/sealet/sealet.entity.repository.go
@@ -4,4 +4,5 @@ import "context"
 
 type Repository interface {
 	Now(ctx context.Context, req *NowRequest) (resp *NowResponse, err error)
+	NowError(ctx context.Context, req *NowErrorRequest) (resp *NowErrorResponse, err error)
 }
