27c938207bd74bb4c037933fae09ef04fe47cb9a
kaydxh <523520531@qq.com>
2023-02-15 17:32:49 +0800

    feat(script): add script for delete project

---
+++ script/delete-project.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# fail on any error
+set -euo pipefail
+#set -o xtrace
+
+SEA_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+PROJECT_NAME=
+
+help() {
+    echo "Usage:"
+    echo "./delete-project.sh [-t target]"
+    echo "Description:"
+    echo "target, the name of delete project."
+    exit -1
+}
+
+while getopts 't:' option; do
+  case ${option} in
+    t) PROJECT_NAME=${OPTARG};;
+    ?) help ;;
+  esac
+done
+
+function usage() {
+    echo >&2 "Usage: $0 NEW_PROJECT_NAME"
+}
+
+function checkParams {
+  if [[ -z "${NEW_PROJECT_NAME}" || -z "${NEW_GIT_REPOSITORY_NAME}" ]]; then
+    help
+    exit 1
+  fi
+}
+
+function deleteProject() {
+  for it in $(find . ! -path "*third_party*" ! -path "*node_modules*" -type d -path "*${PROJECT_NAME}*")
+  do
+   echo "delete dir: ${it}" 
+   rm -rf ${it}
+  done
+}
+
+deleteProject
