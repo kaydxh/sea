f08636c3bfca992433a54c8e5e94e37bb53744c9
kaydxh <xiao_hua_ding@sina.cn>
2023-03-13 09:06:39 +0800

    feat(seadate): update new command for webserver

--- cmd/seadate/app/server.go
+++ cmd/seadate/app/server.go
@@ -31,57 +31,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// NewSeaDateCommand creates a *cobra.Command object with default parameters
-func NewSeaDateCommand(ctx context.Context) *cobra.Command {
-	name := app_.GetVersion().AppName
-	cmd := &cobra.Command{
-		Use:   name,
-		Short: fmt.Sprintf("%s Public HTTP/2 and GRPC APIs", name),
-		// stop printing usage when the command errors
-		Long: fmt.Sprintf(`%s is a gateway serve which you can use curl over HTTP 1.1 or grpc protocal on the same host:port.
-Example: curl -X POST -k https://localhost:port/Now
-See [Sea](https://github.com/kaydxh/sea/blob/master/README.md) for more information.`, name),
-		//SilenceUsage: true,
-
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return runCommand(ctx, cmd)
-		},
-
-		PostRunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Printf("server exit")
-			return nil
-		},
-
-		Args: func(cmd *cobra.Command, args []string) error {
-			for _, arg := range args {
-				if len(arg) > 0 {
-					//%q a single-quoted character literal safely escaped with Go syntax
-					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
-				}
-			}
-			return nil
-		},
-	}
-
-	appFlag := app_.NewAppFlags(cmd)
-	appFlag.Install()
-
-	/*
-		var cfgFile string
-		cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", defaultConfigPath(),
-			fmt.Sprintf("Config file (default is %q)", defaultConfigPath()))
-	*/
-
-	/*
-		cmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-			cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-				fmt.Printf("FLAG: --%s=%q\n", flag.Name, flag.Value)
-			})
-			return nil
-		}
-	*/
-
-	return cmd
+// NewCommand creates a *cobra.Command object with default parameters
+func NewCommand(ctx context.Context) *cobra.Command {
+	return app_.NewCommand(ctx, runCommand)
 }
 
 func runCommand(ctx context.Context, cmd *cobra.Command) error {
